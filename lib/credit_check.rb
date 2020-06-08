d6db35629b8993f77742cd016fa285163f350ac4
Angela Guardia <angelaguardia20@gmail.com>
2020-06-07 21:14:37 -0700

    Update Pseudocode and Add Iteration #1

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,6 +1,38 @@
-card_number = "5541808923795240"
+card_number = "6011797668867828"
 
+card_number_integers = card_number.split(//).map do |digit|
+  digit.to_i
+end
+
+doubled_card_number = card_number_integers.map.with_index do |digit, index|
+  if index.even?
+    digit * 2
+  else
+    digit
+  end
+end
+
+summed_card_number = doubled_card_number.map do |digit|
+  if digit > 9
+    split_digit = digit.to_s.split(//).map {|dig| dig.to_i}
+    split_digit.reduce(0) {|sum, d| sum += d}
+  else
+    digit
+  end
+end
+
+results_summed = summed_card_number.reduce(0) {|sum, digit| sum += digit}
+
+if results_summed % 10 == 0
+  puts "The number #{card_number} is valid!"
+else
+  puts "The number #{card_number} is invalid!"
+end
+
+
+#require 'pry'; binding.pry
 # Your Luhn Algorithm Here
+
 
 # Output
 ## If it is valid, print "The number [card number] is valid!"
---
+++ lib/pseudocode.md
@@ -0,0 +1,14 @@
+##Credit Check
+
+### Iteration 1
+
+- make the card number string into and array of integers
+- make a 'doubled' array by
+    - iterating over card number array (use map)
+    - doubling every other digit
+- create a summed digit array by
+    - detecting integers with double digits
+    - splitting them to single digits
+    - adding them together
+    - adding them to array
+- sum the last array, check validity and print Output
